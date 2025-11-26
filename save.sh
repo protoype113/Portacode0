ed590e05af89e7cfe5a2699d6e127dfca7b2d166
protoype113 <drewclark702@gmail.com>
2025-11-26 02:30:00 -0500

    codespace version 2

--- save.sh
+++ save.sh
@@ -1,6 +1,6 @@
-counter=$(cat count.txt)
+counter=$(cat .count.txt)
 count=$((counter + 1))
-echo "$count" > count.txt
+echo "$count" > .count.txt
 git add .
-git commit -m "codespace version $(cat count.txt)"
+git commit -m "codespace version $(cat .count.txt)"
 git push origin main
