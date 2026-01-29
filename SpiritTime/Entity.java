14ff7974c30d6ec174947996b50f7c43e16cefad
protoype113 <drewclark702@gmail.com>
2026-01-29 10:28:05 -0500

    codespace version 11

--- SpiritTime/Entity.java
+++ SpiritTime/Entity.java
@@ -3,11 +3,15 @@ public class Entity {
     // Variables
     private String name;
     private String id;
+    private Entity prev;
+    private Entity next;
 
     // Workhorse Constructor 
     public Entity(String name, String id) {
         this.name = name;
         this.id = id;
+        prev = null;
+        next = null;
     }
 
     // Getters
@@ -17,5 +21,14 @@ public class Entity {
 
     public String getID() {
         return this.id;
+    }
+
+    // Setters
+    public void setPrev(Entity ent) {
+        this. prev = ent;
+    }
+
+    public void setNext(Entity ent) {
+        this.next = ent;
     }
 }
--- SpiritTime/Main.java
+++ SpiritTime/Main.java
@@ -2,15 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner scan = new Scanner(System.in);
+    public static Entity[] dex = new Entity[30];
+    
 
     public static void main (String[] args) {
-        // Entity a = new Entity("Pikachu", "001");
-        // Entity b = new Entity("Gyarados", "002");
-        // Entity[] team = {a, b};
-        // User man = new User("Gary", team);
-        // System.out.println(man.getName());
-        // man.listContained();
-
+        initializeDex();
         clearConsole();
         System.out.println();
         System.out.println("Welcome to ERN, the Entity Routing Network.");
@@ -20,24 +16,26 @@ public class Main {
         clearConsole();
         System.out.println();
         while (true) {
-            System.out.println("Options");
+            System.out.println("options");
             System.out.println("1. Locate New Entities");
             System.out.println("2. Interact With Contained Entities");
             System.out.println("3. Agency Entity Manual");
+            System.out.println("4. Exit Program");
             String a = scan.next();
+            clearConsole();
+            System.out.println();
             if (a.equals("1")) {
                 encounter();
             } else if (a.equals("2")) {
                 caught();
             } else if (a.equals("3")) {
                 manual();
-            } else if (a.equals("c")) {
+            } else if (a.equals("4")) {
                 break;
             } else {
-                clearConsole();
-                System.out.println();
-                System.out.print("Invalid, please select another ");
+                System.out.print("Invalid, please select one of the valid ");
             }
+            clearConsole();
         }
     }
 
@@ -68,6 +66,47 @@ public class Main {
     }
 
     public static void manual() {
-        System.out.println("C");
+        System.out.println("Accessing Dataase:  Search for entity by name or id");
+        String search = scan.next();
+        for (Entity e : dex) {
+            if (search.equals(e.getID()) || search.equals(e.getName())) {
+                System.out.println(e.getID() + " : " + e.getName());
+            }
+        }
+        search = scan.nextLine();
+        hold();
+    }
+
+    public static void initializeDex() {
+        dex[0] = new Entity("Ashiarai yashiki", "001");
+        dex[1] = new Entity("Baba Yaga", "002");
+        dex[2] = new Entity("Chupacabra", "003");
+        dex[3] = new Entity("Dullahan", "004");
+        dex[4] = new Entity("Efreti", "005");
+        dex[5] = new Entity("Fenrir", "006");
+        dex[6] = new Entity("Gashadokuro", "007");
+        dex[7] = new Entity("Huldra", "008");
+        dex[8] = new Entity("Itsumade", "009");
+        dex[9] = new Entity("Jiangshi", "010");
+        dex[10] = new Entity("Kelpie", "011");
+        dex[11] = new Entity("Leshy", "012");
+        dex[12] = new Entity("Minotaur", "013");
+        dex[13] = new Entity("Nue", "014");
+        dex[14] = new Entity("Oni", "015");
+        dex[15] = new Entity("Peryton", "016");
+        dex[16] = new Entity("Qilin", "017");
+        dex[17] = new Entity("Raiju", "018");
+        dex[18] = new Entity("Skinwalker", "019");
+        dex[19] = new Entity("Tengu", "020");
+        dex[20] = new Entity("Undine", "021");
+        dex[21] = new Entity("Valkyrie", "022");
+        dex[22] = new Entity("Wendigo", "023");
+        dex[23] = new Entity("Xuehe", "024");
+        dex[24] = new Entity("Yuki-onna", "025");
+        dex[25] = new Entity("Ziz", "026");
+        dex[26] = new Entity("Anubis", "027");
+        dex[27] = new Entity("Banshee", "028");
+        dex[28] = new Entity("Chimera", "029");
+        dex[29] = new Entity("Dybbuk", "030");
     }
 }
